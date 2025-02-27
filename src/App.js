e6e80fe59a1afbb83aeb8ad34821dd3611145e38
arfao <arfaouizaineb43@gmail.com>
2025-02-27 19:10:30 +0100

    Correction

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
-<<<<<<< HEAD
 import Navbar from './components/navbar/Navbar';
-import './App.css';
 import Header from './components/header/Header';
 import AboutPage from './components/test/AboutPage';
-=======
-import '@coreui/coreui/dist/css/coreui.min.css';
-import Navbar from './components/navbar/Navbar';
-import './App.css';
-import Header from './components/header/Header';
 import Part from './components/Partners/Part';
 import AboutATIA from './components/aboutATIA/AboutATIA';
-import Bureau from './components/bureau/Bureau'
-import Events from './components/events/Events'
-import Footer from './components/footer/Footer'
->>>>>>> 4eabc10 (J'ai complété tous le components de projet)
+import Bureau from './components/bureau/Bureau';
+import Events from './components/events/Events';
+import Footer from './components/footer/Footer';
+import '@coreui/coreui/dist/css/coreui.min.css';
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
-<<<<<<< HEAD
       <AboutPage />
-=======
       <Part />
-      <AboutATIA/>
-      <Bureau/>
-      <Events/>
-      <Footer/>
->>>>>>> 4eabc10 (J'ai complété tous le components de projet)
+      <AboutATIA />
+      <Bureau />
+      <Events />
+      <Footer />
     </div>
   );
 };
